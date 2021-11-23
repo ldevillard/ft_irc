db32919ee95ff0d9c74de01a6273f1891954556d
Gauthier Poulain <gapoulai@student.42lyon.fr>
2021-11-23 16:07:20 +0100

    client qui bug mais ok

--- client/client.py
+++ client/client.py
@@ -1,6 +1,17 @@
 import tkinter as tk
 
 
+class Server:
+    _address: str
+    _port: int
+    _nickname: str
+    _username: str
+    _password: str
+
+    def __init__(self):
+        pass
+
+
 def connect():
     master = tk.Tk()
     master.title("ft_irc : connect")
@@ -11,17 +22,43 @@ def connect():
     tk.Label(master, text="Username").grid(row=3, column=0)
     tk.Label(master, text="Password").grid(row=4, column=0)
 
-    address = tk.Entry(master).grid(row=0, column=1)
-    port = tk.Entry(master).grid(row=1, column=1)
-    nickname = tk.Entry(master).grid(row=2, column=1)
-    username = tk.Entry(master).grid(row=3, column=1)
-    password = tk.Entry(master, show='*').grid(row=4, column=1)
+    address = tk.Entry(master)
+    port = tk.Entry(master)
+    nickname = tk.Entry(master)
+    username = tk.Entry(master)
+    password = tk.Entry(master, show='*')
 
-    tk.Button(master, text="Connect", command=master.quit).grid(
+    address.grid(row=0, column=1)
+    port.grid(row=1, column=1)
+    nickname.grid(row=2, column=1)
+    username.grid(row=3, column=1)
+    password.grid(row=4, column=1)
+
+    formData: Server = Server()
+
+    def fetchForm():
+        formData._address = address.get()
+        formData._port = port.get()
+        formData._nickname = nickname.get()
+        formData._username = username.get()
+        formData._password = username.get()
+        master.quit()
+
+    tk.Button(master, text="Connect", command=fetchForm).grid(
         row=5, column=0, sticky=tk.W, pady=4)
 
-    tk.mainloop()
+    master.mainloop()
+
+    return formData
+
+
+def mainFrame(server: Server):
+    master = tk.Tk()
+    master.title("ft_irc : connected to "+server._address+":"+server._port)
+
+    master.mainloop()
 
 
 if __name__ == '__main__':
-    connect()
+    server: Server = connect()
+    mainFrame(server)
