789b3ca44fc92c2fcd6c95ef2205ef40bdfdad1f
neryss <neryssistos@gmail.com>
2021-11-24 20:52:38 +0100

    Working prototype yikes !

--- srcs/Server/servData.cpp
+++ srcs/Server/servData.cpp
@@ -1,4 +1,5 @@
 #include "servData.hpp"
+#include <csignal>
 
 ServData::~ServData()
 {
@@ -11,6 +12,7 @@ ServData::ServData() : _msg("Salut les gars\n"), _addlen(sizeof(_address)), _opt
 
 void ServData::init()
 {
+	// std::signal(SIGPIPE, SIG_IGN);
 	if (!(_server_fd = socket(AF_INET, SOCK_STREAM, 0)))
 		throw ServerException::socket_creation();
 	if (setsockopt(_server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &_opt, sizeof(_opt)))
@@ -24,6 +26,7 @@ void ServData::init()
 		throw ServerException::listening();
 	if ((_new_socket = accept(_server_fd, (struct sockaddr *)&_address, (socklen_t *)&_addlen)) < 0)
 		throw ServerException::receiving();
+	std::cout << "Init done" << std::endl;
 }
 
 
@@ -32,10 +35,25 @@ int	ServData::connect()
 	for (;;)
 	{
 		bzero(_buffer, sizeof(_buffer));
-		_valread = read(_new_socket, _buffer, sizeof(_buffer));
-		std::cout << "current buffer : " << _buffer << std::endl;
-		send(_new_socket, _msg.c_str(), _msg.length(), 0);
-		std::cout << "Message sent!" << std::endl;
+		_valread = recv(_new_socket, _buffer, sizeof(_buffer), 0);
+		if (_valread > 0)
+		{
+			std::cout << _buffer << std::endl;
+			bzero(_buffer, sizeof(_buffer));
+			send(_new_socket, _buffer, sizeof(_buffer), 0);
+			// send(_new_socket, _msg.c_str(), _msg.length(), 0);
+			// recv(_new_socket, _buffer, sizeof(_buffer), 0);
+			std::cout << "Message sent!" << std::endl;
+		}
 	}
+	close(_new_socket);
+	// while (_new_socket > 0)
+	// {
+	// 	bzero(_buffer, sizeof(_buffer));
+	// 	// send(_new_socket, _buffer, sizeof(_buffer), 0);
+	// 	// send(_new_socket, _msg.c_str(), _msg.length(), 0);
+	// 	recv(_new_socket, _buffer, sizeof(_buffer), 0);
+	// 	std::cout << _buffer << std::endl;
+	// }
 	return (0);
 }
