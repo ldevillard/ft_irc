5ff3498f967d230649f04c6b39352aeb200ddab1
neryss <neryssistos@gmail.com>
2021-11-25 16:55:22 +0100

    cleaned up

--- src/servData.cpp
+++ src/servData.cpp
@@ -19,7 +19,7 @@ ServData::ServData(id_t port) : _msg("Salut les gars\n"), _addlen(sizeof(_addres
 
 void ServData::init()
 {
-	// std::signal(SIGPIPE, SIG_IGN);
+	std::signal(SIGPIPE, SIG_IGN);
 	if (!(_server_fd = socket(AF_INET, SOCK_STREAM, 0)))
 		throw ServerException::socket_creation();
 	if (setsockopt(_server_fd, SOL_SOCKET, SO_REUSEADDR, &_opt, sizeof(_opt)))
@@ -66,27 +66,16 @@ int ServData::connect()
 					std::cout << "Connection refused: username invalid!" << std::endl;
 				else if (i == 2)
 					std::cout << "Connection refused: nickname invalid!" << std::endl;
-
 				break;
 			}
 
 			std::cout << _buffer;
 			bzero(_buffer, sizeof(_buffer));
 			send(_client_socket, _buffer, _valread + 1, 0);
-			// send(_new_socket, _msg.c_str(), _msg.length(), 0);
-			// recv(_new_socket, _buffer, sizeof(_buffer), 0);
 		}
 
 		std::cout << "Nickname : " + user.getNick() + " and Username : " + user.getUser() << std::endl;
 	}
-	close(_server_fd);
-	// while (_new_socket > 0)
-	// {
-	// 	bzero(_buffer, sizeof(_buffer));
-	// 	// send(_new_socket, _buffer, sizeof(_buffer), 0);
-	// 	// send(_new_socket, _msg.c_str(), _msg.length(), 0);
-	// 	recv(_new_socket, _buffer, sizeof(_buffer), 0);
-	// 	std::cout << _buffer << std::endl;
-	// }
+	close(_client_socket);
 	return (0);
 }
