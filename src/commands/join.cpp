d3054b451f45bdd29bef13b2cea9d945adaa133e
Logan Devillard <logandvllrd@gmail.com>
2021-11-30 15:03:43 +0100

    testing how channel works with join

--- src/commands/join.cpp
+++ src/commands/join.cpp
@@ -1,5 +1,7 @@
 #include "../../includes/commands/join.hpp"
 #include <iostream>
+#include "../../includes/channel.hpp"
+#include "../../includes/rpl_codes.hpp"
 
 Join::Join(User *user) : Command("JOIN", "<channel> <password> : Join wanted channel.", user)
 {
@@ -7,5 +9,16 @@ Join::Join(User *user) : Command("JOIN", "<channel> <password> : Join wanted cha
 
 void Join::execute()
 {
-	std::cout << _user->getNick() + ": Executing Join!" << std::endl;
+	if (_args[1] != "#")
+	{
+		channel *chan = new channel(_args[1]);
+
+		chan->broadcastMsg(_args[1]);
+		sendMsgToUser(_user, response(RPL_INFO, _user->getNick(), "", ":joining " + _args[1] + " channel"));
+		sendMsgToUser(_user, _user->getNick() + "!" + _user->getNick() + "@127.0.0.1 JOIN" + _args[1]);
+
+		sendMsgToUser(_user, ":127.0.0.1 332 gauthier "+ _args[1] + " :generic channel");
+		sendMsgToUser(_user, ":127.0.0.1 353 gauthier = " + _args[1] + " :@" + _user->getNick());
+		//sendMsgToUser(_user, ":127.0.0.1 366 gauthier #salut :End of NAMES list");
+	}
 }
