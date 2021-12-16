c2ed2bef3cd86442bde7a64490d35aff91998a92
gauthier poulain <poulain.gauthier16@gmail.com>
2021-12-16 15:01:06 +0100

    fix list for hexchat

--- src/commands/list.cpp
+++ src/commands/list.cpp
@@ -15,24 +15,24 @@ std::string itoa(int nb)
 
 void List::execute()
 {
-    std::map<std::string, Channel*>::iterator it;
+	std::map<std::string, Channel *>::iterator it;
 
-    
-    if (_args.size() > 1)
-    {
-        Channel *chan = _server->findChannel(_args[1]);
-        if (chan == NULL)
-            _user->sendMsg(":127.0.0.1 " + std::string(ERR_NOSUCHCHANNEL) + " " + _args[1] + ": No such channel!");
-        else
-            _user->sendMsg(std::string(RPL_LIST) + " * " + chan->getName() + " " + itoa(chan->getMembers().size())); //TO STRING
-    }
-    else
-    {
-        for (it = _server->getChannels().begin(); it != _server->getChannels().end(); it++)
-        {
-            _user->sendMsg(std::string(RPL_LIST) + " * " + (*it).second->getName() + " " + itoa((*it).second->getMembers().size())); //TO STRING
-        }
-    }
-    _user->sendMsg(std::string(RPL_LISTEND));
-    
+	if (_args.size() > 1)
+	{
+		Channel *chan = _server->findChannel(_args[1]);
+		if (chan == NULL)
+			_user->sendMsg(":127.0.0.1 " + std::string(ERR_NOSUCHCHANNEL) + " " + _args[1] + ": No such channel!");
+		else
+		{
+			_user->sendMsg(":127.0.0.1 " + std::string(RPL_LIST) + " * " + (*it).second->getName() + " " + itoa((*it).second->getMembers().size())); //TO STRING
+		}
+	}
+	else
+	{
+		for (it = _server->getChannels().begin(); it != _server->getChannels().end(); it++)
+		{
+			_user->sendMsg(":127.0.0.1 " + std::string(RPL_LIST) + " * " + (*it).second->getName() + " " + itoa((*it).second->getMembers().size())); //TO STRING
+		}
+	}
+	_user->sendMsg(":127.0.0.1 " + std::string(RPL_LISTEND) + " :End of LIST");
 }
